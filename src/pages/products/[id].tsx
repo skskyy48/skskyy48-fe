478fe779c02e33b7cdeb5123e8c70e2e1786b1a4
skskyy48 <poiuy5958@gmail.com>
2023-03-21 14:35:56 +0900

    add get product info

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,27 +1,52 @@
 import Link from 'next/link';
-import type { NextPage } from 'next';
-import React from 'react';
+import type { NextPage, NextPageContext } from 'next';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-import products from '../../api/data/products.json';
+import { Product } from '../../types/product';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { numberWithCommas } from '../../utilities';
+import { useSelector } from 'react-redux';
+import { RootState } from '../_app';
+import MyHeader from '../../components/MyHeader';
 
 const ProductDetailPage: NextPage = () => {
-  const product = products[0];
+  const [product,setProduct] = useState<Product>({
+    name : '',
+    id : '',
+    thumbnail : '',
+    price : 0
+  });
+  const router = useRouter();
+  const {id} = router.query
+  const isLogin = useSelector((state:RootState) => state.info.isLogin)
+  const user = useSelector((state:RootState) => state.info.name);
+
+  useEffect(() => {
+    if(typeof id !== 'undefined') getInfo(id.toString())
+  },[])
+
+  const getInfo = async (id:string) => {
+    try {
+      const data = await axios.get(`https://api.sixshop.com/products/${id}`)
+
+      if(data.status === 200){
+        setProduct(data.data.data.product)
+      }else{
+        router.push('/404')
+      }
+    } catch {
+      router.push('/404')
+    }
+  }
 
   return (
     <>
-      <Header>
-        <Link href='/'>
-          <Title>HAUS</Title>
-        </Link>
-        <Link href='/login'>
-          <p>login</p>
-        </Link>
-      </Header>
-      <Thumbnail src={product.thumbnail ? product.thumbnail : '/defaultThumbnail.jpg'} />
+      <MyHeader />
+      <Thumbnail src={product?.thumbnail ? product.thumbnail : '/defaultThumbnail.jpg'} />
       <ProductInfoWrapper>
-        <Name>{product.name}</Name>
-        <Price>{product.price}원</Price>
+        <Name>{product?.name}</Name>
+        <Price>{numberWithCommas(product?.price)}원</Price>
       </ProductInfoWrapper>
     </>
   );
@@ -59,3 +84,10 @@ const Price = styled.div`
   font-size: 18px;
   margin-top: 8px;
 `;
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const { query } = context;
+  const { id } = query;
+  if(typeof id !== 'undefined') return { props: { id } };
+  else return {props: {id : 1}}
+};
