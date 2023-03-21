50c30a33df2378144c3f623781fbd564a4d5fbd4
skskyy48 <poiuy5958@gmail.com>
2023-03-21 14:35:33 +0900

    fix index list, add api etc...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,74 @@
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-import products from '../api/data/products.json';
 import ProductList from '../components/ProductList';
 import Pagination from '../components/Pagination';
+import { useDispatch } from 'react-redux';
+import { setInfo, setIsLogin } from '../reducers/info';
+import {NextPageContext} from 'next/types'
+import axios from 'axios';
+import MyHeader from '../components/MyHeader';
 
 const HomePage: NextPage = () => {
+  const [usePage,setUsePage] = useState<string|string[]>('1');
+  const [products,setProducts] = useState([]);
+  const [totalCount,setTotalCount] = useState(0);
+
   const router = useRouter();
   const { page } = router.query;
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+      if(typeof page !== 'undefined'){
+        setUsePage(page)
+      }
+      
+      getProducts()
+      let token = localStorage.getItem('token');
+      
+      if(token !== null){
+        let info = localStorage.getItem('userInfo');
+        if(info !== null){
+          let i = JSON.parse(info)
+          dispatch(setInfo(i))
+          dispatch(setIsLogin(true))
+        }
+      }
+  }, [])
+
+  useEffect(() => {
+    getProducts()
+  },[usePage])
+
+  const getProducts = async() => {
+    try {
+      const data = await axios.get(`https://api.sixshop.com/products?page=${usePage}&size=10`)
+
+      if(data.status === 200){
+        setProducts(data.data.data.products)
+        setTotalCount(data.data.data.totalCount)
+      }else {
+        router.push('/404')
+      }
+    } catch {
+      router.push('/404')
+    }
+  }
+  
+  const onClickItem = (id:number) => {
+    router.push({
+      pathname : '/products/[id]',
+      query : { id : id }
+    })
+  }
 
   return (
     <>
-      <Header>
-        <Link href='/'>
-          <Title>HAUS</Title>
-        </Link>
-        <Link href='/login'>
-          <p>login</p>
-        </Link>
-      </Header>
+      <MyHeader />
       <Container>
-        <ProductList products={products.slice(0, 10)} />
-        <Pagination />
+        <ProductList products={products} clickItem={onClickItem}/>
+        <Pagination page={Number(usePage)} setPage={setUsePage} total={totalCount}/>
       </Container>
     </>
   );
@@ -49,3 +93,10 @@ const Container = styled.div`
   align-items: center;
   padding: 0 20px 40px;
 `;
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const { query } = context;
+  const { page } = query;
+  if(typeof page !== 'undefined') return { props: { page } };
+  else return {props: {page : 1}}
+};
